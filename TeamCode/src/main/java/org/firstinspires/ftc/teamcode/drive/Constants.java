834c76eee7a5adf729d11faddca278b9419fb2fa
mrjerm <79760703+mrjerm@users.noreply.github.com>
2022-02-12 10:36:17 -0500

    git commit -m "poggers"

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/Constants.java
@@ -0,0 +1,24 @@
+package org.firstinspires.ftc.teamcode.drive;
+
+public class Constants {
+    public static double clawOpenPos = 0.28;
+    public static double clawClosePos = 0.08;
+    public static double clawRestPos = 0.15;
+    public static double horizontalSpeedLimit = 0.8;
+    public static double DR4B_Rest = 0;
+    public static double DR4B_Low = 0.22;
+    public static double DR4B_Mid = 0.35;
+    public static double DR4B_High = 0.7;
+    public static double DR4B_Cap = 1;
+    public static double DR4B_Clear = DR4B_Mid;
+    public static double buttonBufferTime = 0.3;
+    public static double salamiSpeed = 0.6;
+    public static double odometerUpPos = 0.87;
+    public static double odometerDownPos = 0.2;
+
+    /**TODO: set these variables */
+    public static int horizontalSlideClear = 968;
+    public static int horizontalSlideL1 = 1100;
+    public static int horizontalSlideL2 = 1400;
+    public static int horizontalSlideL3 = 1400;
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/ColorTest.java
@@ -0,0 +1,51 @@
+package org.firstinspires.ftc.teamcode.drive.opmode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+
+@TeleOp
+public class ColorTest extends OpMode {
+
+    public ColorSensor colorSensor;
+    public DcMotorEx intake;
+    public double time = 0;
+    public boolean freightDetected = false;
+    public boolean readyToLift = false;
+    public boolean timeResetted = false;
+    public boolean freightInGrabber = false;
+
+    @Override
+    public void init() {
+        intake = hardwareMap.get(DcMotorEx.class, "Motor Intake");
+        intake.setDirection(DcMotorSimple.Direction.FORWARD);
+
+        colorSensor = hardwareMap.get(ColorSensor.class, "Color Sensor");
+    }
+
+    @Override
+    public void loop() {
+        if (colorSensor.red() > 400){
+            freightDetected = true;
+        }
+        else if (colorSensor.red() < 400){
+            freightDetected = false;
+        }
+        if (freightDetected){
+            intake.setPower(-0.8);
+        }
+        else if (!freightDetected && gamepad2.right_bumper){
+            intake.setPower(0.8);
+        }
+        else if (!freightDetected && !gamepad2.right_bumper){
+            intake.setPower(0);
+        }
+        telemetry.addData("red value", colorSensor.red());
+        telemetry.addData("freight detected?", freightDetected);
+        telemetry.update();
+    }
+}
